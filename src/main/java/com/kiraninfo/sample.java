f00a41db9ee43cc5ddfb43f053ab31d03561dc0a
KiranNandeshwar <kiran.kirya29@gmail.com>
2023-03-20 13:09:33 +0000

    Update 3

---
+++ src/main/java/com/kiraninfo/sample.java
@@ -0,0 +1,14 @@
+package com.kiraninfo;
+
+/**
+ * Hello world!
+ *
+ */
+public class sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Guys!" );
+    }
+}
+
