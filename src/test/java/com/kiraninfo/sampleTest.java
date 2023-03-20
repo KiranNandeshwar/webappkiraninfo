382acbfea4b5f898f5e1d346cc41885d1cc421a8
KiranNandeshwar <kiran.kirya29@gmail.com>
2023-03-20 13:26:10 +0000

    update 5

---
+++ src/test/java/com/kiraninfo/sampleTest.java
@@ -0,0 +1,21 @@
+package com.kiraninfo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class sampleTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
+
